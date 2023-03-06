64002ce2318111931c9a92812633aee862161aea
Saileela <shashank.20bcb7105@vitap.ac.in>
2023-03-06 23:46:26 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import os
-import shutil
+try:
 
-from enum import Enum
-from io import BytesIO, StringIO
-from typing import Union
+    from enum import Enum
+    from io import BytesIO, StringIO
+    from typing import Union
+    import os
 
-import pandas as pd
-import streamlit as st
+    import pandas as pd
+    import streamlit as st
+except Exception as e:
+    print(e)
 
 STYLE = """
 <style>
@@ -16,13 +18,22 @@ img {
 </style>
 """
 
+UPLOAD_DIRECTORY = "uploads"
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
 
 class FileUpload(object):
 
-    def __init__(self, upload_dir='uploads'):
+    def __init__(self):
         self.fileTypes = ["csv", "png", "jpg", "jpeg", "mp4", "webm", "ogg"]
-        self.upload_dir = upload_dir
-        os.makedirs(upload_dir, exist_ok=True)
+
+    def save_file(self, file):
+        file_path = os.path.join(UPLOAD_DIRECTORY, file.name)
+        with open(file_path, "wb") as f:
+            f.write(file.getbuffer())
+        return file_path
 
     def run(self):
         """
@@ -38,36 +49,46 @@ class FileUpload(object):
         selected_files = []
         for file in files:
             content = file.getvalue()
-            filepath = os.path.join(self.upload_dir, file.name)
-            with open(filepath, 'wb') as f:
-                f.write(content)
             if file.type.startswith('image/'):
                 st.image(file, use_column_width=True)
             elif file.type.startswith('video/'):
                 st.video(file)
             elif file.type == 'text/csv':
-                data = pd.read_csv(BytesIO(content))
+                data = pd.read_csv(file)
                 st.dataframe(data.head(10))
             else:
                 st.warning("Unsupported file type: " + file.type)
                 continue
-            if st.checkbox("Download " + file.name):
-                selected_files.append((file, filepath))
-        if st.button("Download All"):
-            for file, filepath in selected_files:
-                if file.type.startswith('image/'):
-                    st.image(file, use_column_width=True)
-                elif file.type.startswith('video/'):
-                    st.video(file)
-                elif file.type == 'text/csv':
-                    st.download_button(
-                        label="Download " + file.name,
-                        data=open(filepath, 'rb').read(),
-                        file_name=file.name,
-                        mime="text/csv"
-                    )
+            if st.checkbox("Save " + file.name):
+                file_path = self.save_file(file)
+                selected_files.append(file_path)
+        if selected_files:
+            st.success("Saved files:")
+            for file_path in selected_files:
+                st.write(file_path)
+                file_name = os.path.basename(file_path)
+                mime_type = "auto"
+                if file_name.endswith(".csv"):
+                    mime_type = "text/csv"
+                elif file_name.endswith(".jpg") or file_name.endswith(".jpeg"):
+                    mime_type = "image/jpeg"
+                elif file_name.endswith(".png"):
+                    mime_type = "image/png"
+                elif file_name.endswith(".mp4"):
+                    mime_type = "video/mp4"
+                elif file_name.endswith(".webm"):
+                    mime_type = "video/webm"
+                elif file_name.endswith(".ogg"):
+                    mime_type = "audio/ogg"
+                st.download_button(
+                    label="Download " + file_name,
+                    data=open(file_path, "rb").read(),
+                    file_name=file_name,
+                    mime=mime_type
+                )
 
 
 if __name__ == "__main__":
     helper = FileUpload()
     helper.run()
+
